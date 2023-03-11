ebcc2166c0d47619366566a08b13e9b11926ba24
dstokesy <danielstokes93@gmail.com>
2023-03-11 12:21:25 +0000

    updates styling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,7 +10,7 @@ export const Header: React.FC = () => {
             <div className="container mx-auto">
                 <div className="grid grid-cols-12 gap-4 items-center">
                     <div className="hidden sm:block sm:col-start-1 sm:col-end-7 md:cols-end-5">
-                        <div className={cx(styles.logo, 'uppercase')}>
+                        <div className={styles.logo}>
                             <Link href="/">Movie DB</Link>
                         </div>
                     </div>
